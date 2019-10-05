daf5bad4cb75b079735918f36c63039527336825
Danilo Lira <drla@cin.ufpe.br>
2019-10-04 22:59:56 -0300

    Criação da página das vítimas e adição da tab

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},  { path: 'formulario', loadChildren: './formulario/formulario.module#FormularioPageModule' },
+  { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'vitimas', loadChildren: './vitimas/vitimas.module#VitimasPageModule' },
 
 ];
 
---
+++ app/src/app/vitimas/vitimas.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { VitimasPage } from './vitimas.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: VitimasPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [VitimasPage]
+})
+export class VitimasPageModule {}
