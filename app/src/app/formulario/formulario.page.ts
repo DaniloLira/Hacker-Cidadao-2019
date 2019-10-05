2197408c776bc380fae00cb77acf5513c240e49f
nicoedu <nicoedu_@hotmail.com>
2019-10-05 03:26:33 -0300

    layout form

--- app/src/app/formulario/formulario.page.ts
+++ app/src/app/formulario/formulario.page.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-formulario',
-  templateUrl: './formulario.page.html',
-  styleUrls: ['./formulario.page.scss'],
+  selector: "app-formulario",
+  templateUrl: "./formulario.page.html",
+  styleUrls: ["./formulario.page.scss"]
 })
 export class FormularioPage implements OnInit {
-
-  constructor() { }
+  tipoAcidente: String;
+  tipoVeiculoI: String;
+  tipoVeiculoV: String;
+  airbag: String;
+  capacete: String;
+  cinto: String;
+  constructor() {}
 
   ngOnInit() {
+    this.tipoAcidente = "0";
+    this.airbag = "0";
+    this.capacete = "0";
+    this.cinto = "0";
   }
 
+  sendForm() {
+    console.log(this.tipoAcidente);
+  }
 }
